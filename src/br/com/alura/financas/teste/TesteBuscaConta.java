98046f7b929e67ff9d86bce4266b086c77d19e9e
Alone Pereira <alone.pereira@hotmail.com>
2017-06-18 23:46:00 -0300

    JPA aula3: Conhecendo o estado Managed

---
+++ src/br/com/alura/financas/teste/TesteBuscaConta.java
@@ -0,0 +1,26 @@
+package br.com.alura.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.financas.modelo.Conta;
+import br.com.alura.financas.util.JPAUtil;
+
+public class TesteBuscaConta {
+
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Conta conta = em.find(Conta.class, 1);
+		conta.setTitular("João");
+		conta.setNumero("456");
+		
+		
+		System.out.println(conta.getTitular());
+		
+		em.getTransaction().commit();
+	}
+	
+}
